410da05a6d7c079a5a14377866c7fc0522d36601
Tiago Amaro <tiagopadrela@gmail.com>
2020-06-05 10:46:10 -0300

    Adding class 2 examples

---
+++ Lógica de Programação/aula_2/conditional-expressions.js
@@ -0,0 +1,11 @@
+let penColor = "black"
+let makeTest = false
+
+if (penColor === "blue") {
+  makeTest = true
+} else {
+  penColor = "blue"
+}
+
+penColor // Should be "blue"
+makeTest // Should be false
